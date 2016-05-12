7ec8c027591d203fd53a8ead6a385b159b89b0fb
Sean Houlihan <sean@seanhweb.com>
2016-05-12 11:23:24 -0700

    Submodule Addition - Barcodes

---
+++ class.inc.php
@@ -0,0 +1,14 @@
+<?php
+    class row {
+        public function __construct($name) {
+            
+        }
+    }
+
+    class row_set {
+        public function __construct($array) {
+            
+        }
+    }
+
+?>
