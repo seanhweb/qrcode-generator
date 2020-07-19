63468a852d1cee36e15577b15fb477cadd051bd6
Sean Houlihan <seanhweb@users.noreply.github.com>
2020-07-18 18:18:38 -0700

    Fixes dangerous use of shell_exec
    Fixes dangerous use of shell_exec and ensures capability with Windows PHP

--- class.inc.php
+++ class.inc.php
@@ -7,9 +7,15 @@
 
     class FS_Operation {
         public static function clear_tmp() {
-            shell_exec('rm -rf '.getcwd().'/img/tmp/*'); 
+            $files = glob(__DIR__.'/img/tmp/*');
+            foreach($files as $file) { 
+                if(is_file($file)) {
+                  unlink($file);
+                }
+            }
         }
     }
+
     class Tag {
         public function __construct($package_name, $package_number, $serial_number, $edmc, $items)  {
             include('templates/tag.tpl.php'); 
